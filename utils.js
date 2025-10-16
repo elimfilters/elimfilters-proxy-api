6289e41f290c494aa0d5c6ce8727fa1959897e6c
elimfilters <vabreu@elimfilters.com>
2025-10-16 01:20:46 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,11 @@
 // utils.js
 
-// Ambas funciones deben ser exportadas
+// Funciones de Nodemailer aquí. Asumimos que los detalles de nodemailer ya se configuraron.
+
 export function logWebhookActivity(req, statusCode, startTime, resultOrError) {
-    // ...
+    // ... (lógica de logging)
 }
 
 export async function sendCriticalAlert(to, subject, body) {
-    // ...
+    // ... (lógica de envío de email)
 }
