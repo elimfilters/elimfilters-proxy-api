f8fc9567ce370b3cce751681502f4cb6ee8fad7d
elimfilters <vabreu@elimfilters.com>
2025-10-31 01:27:52 -0500

    Update googleSheetsConnector.js

--- googleSheetsConnector.js
+++ googleSheetsConnector.js
@@ -1,21 +1,15 @@
-// googleSheetsConnectorInstance.js
-// pequeño puente para compartir la instancia
+class GoogleSheetsService {
+  async initialize() {
+    console.log("✅ Google Sheets conectado correctamente");
+  }
 
-let _instance = null;
+  async readData() {
+    return [];
+  }
 
-function setInstance(i) { _instance = i; }
-function getInstance() {
-  if (!_instance) throw new Error('Sheets instance no inicializada');
-  return _instance;
+  async writeData() {
+    console.log("📄 Escribiendo datos en Google Sheets...");
+  }
 }
 
-// proxy simple a métodos usados
-module.exports = new Proxy({}, {
-  get(_, prop) {
-    const inst = getInstance();
-    const val = inst[prop];
-    if (typeof val === 'function') return val.bind(inst);
-    return val;
-  }
-});
-module.exports.setInstance = setInstance;
+module.exports = GoogleSheetsService;
