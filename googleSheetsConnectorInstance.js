8b4b2d16400616f5498f1598a5c8d20b5cb0fad9
elimfilters <vabreu@elimfilters.com>
2025-10-31 09:11:33 -0500

    Update googleSheetsConnectorInstance.js

--- googleSheetsConnectorInstance.js
+++ googleSheetsConnectorInstance.js
@@ -1,25 +1,22 @@
 // googleSheetsConnectorInstance.js
-// Puente para compartir UNA sola instancia del servicio de Google Sheets.
+// Puente Proxy para compartir UNA instancia del conector (factory.create()).
+
+'use strict';
 
 let _instance = null;
 
-function setInstance(i) {
-  _instance = i;
-}
-
+function setInstance(i) { _instance = i; }
 function getInstance() {
   if (!_instance) throw new Error('Sheets instance no inicializada');
   return _instance;
 }
 
-// Proxy: expone los métodos/props de la instancia real
 module.exports = new Proxy({}, {
   get(_t, prop) {
     const inst = getInstance();
     const val = inst[prop];
-    return typeof val === 'function' ? val.bind(inst) : val;
+    return (typeof val === 'function') ? val.bind(inst) : val;
   }
 });
 
-// Permite inyectar la instancia desde server.js
 module.exports.setInstance = setInstance;
